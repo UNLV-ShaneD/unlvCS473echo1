e79bdf410c28c29b4fb66c9d55550e4236a99fba
Shane D <dieckma3@unlv.nevada.edu>
2012-02-02 12:46:28 -0800

    added webMagicPacket applet
    -sends a Wake on Lan magicPacket to the specified host an IP address

---
+++ webMagicPacket/src/dao/MagicPacketServlet.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MagicPacketServlet
+ */
+@WebServlet("/MagicPacketServlet")
+public class MagicPacketServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MagicPacketServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		WakeOnLan wol = new WakeOnLan();
+		wol.Wake("192.168.1.113", "00:24:8C:9E:17:B4");
+		
+		// Test page output
+		HttpSession session = request.getSession();
+		session.setMaxInactiveInterval(5);
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		
+		out.println("Wake command issued!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ webMagicPacket/src/dao/MagicPacketer.java
@@ -0,0 +1,8 @@
+package dao;
+
+public class MagicPacketer {
+	public void sendPacket()
+	{
+		
+	}
+}
